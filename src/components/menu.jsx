d1f8951bc9f3807e92de8834647b62ed000f0ede
Allan <108568433+EdgarAllan07@users.noreply.github.com>
2024-04-24 18:02:02 -0600

    creando el dashboard para el usuario

---
+++ src/components/menu.jsx
@@ -0,0 +1,13 @@
+"use client"
+import React from 'react'
+import SideBar from './SideBar'
+
+function menu() {
+  return (
+    <div>
+        <SideBar></SideBar>
+    </div>
+  )
+}
+
+export default menu
