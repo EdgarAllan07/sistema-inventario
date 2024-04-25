2bd8791aeaab5760b906f2c6cdf1e0cf5460b857
Allan <108568433+EdgarAllan07@users.noreply.github.com>
2024-04-24 18:09:15 -0600

    agregando el logo de la empresa en el dashboard

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -15,7 +15,10 @@ function SideBar() {
 
 <aside id="sidebar-multi-level-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
    <div class="h-full px-3 py-4 overflow-y-auto bg-[#2E6C37]">
-      <ul class="space-y-2 font-medium">
+      <ul class="space-y-2 font-medium  ">
+      <li>
+            <img src='super-selector-logo-png 1.png'  ></img>
+         </li>
          <li>
             <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                <svg class="w-5 h-5 text-black transition duration-75  group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
