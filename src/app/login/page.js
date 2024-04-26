02d16ee6ccb15cde334e87b747458e9410017503
Edgar Saravia <edgar.saravia@zebedee.io>
2024-04-25 22:19:53 -0600

    Agregando estilos al login completado

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,27 +1,14 @@
 import React from 'react'
-import Layout from './layout.js'
-import Image from 'next/image'
+import Layout from "./layout.js";
+import Login from "@/components/Login"
 
 function page() {
   return (
-    <Layout>
-       <section className='flex '>
-        <div className='w-1/2'>
-            <Image src="/selectos-logo-login.png" 
-             width={900}
-      height={900}/>
-        </div>
-        <div className='w-1/2 bg-white flex-cols items-center justify-center'>
-                <h1 className='text-green-900'>Bienvenido</h1>
-                <label className='d-block'>
-                    Nombre usuario
-                </label>
-                <input type='text' className='shadow'>
-                </input>
-  
-        </div>
-       </section>
-    </Layout>
+    <div>
+      <Layout>
+      <Login></Login>
+      </Layout>
+    </div>
   )
 }
 
---
+++ src/components/Login.jsx
@@ -0,0 +1,60 @@
+"use client"
+import React from "react";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation.js";
+
+function Login() {
+  const router = useRouter()
+
+  const handleChange = (e)=>{
+
+  }
+  
+  const submitFunction =(e)=>{
+    e.preventDefault()
+    router.push("/")
+  }
+
+  return (
+      <section className="flex ">
+        <div className="w-1/2">
+          <img src="/selectos-logo-login.png" className="w-full h-full" />
+        </div>
+        <div className="w-1/2 bg-white flex items-center justify-center">
+          <div>
+            <div className="text-center space-y-4 mb-4 w-full">
+              <h1 className="text-green-900 text-4xl">Bienvenido</h1>
+              <p className="font-semibold text-xl">
+                Ingresa tus credenciales para usar el dashboard
+              </p>
+            </div>
+            <form className="space-y-4" onSubmit={submitFunction}>
+              <input
+                type="text"
+                id="search"
+                className="block w-full p-3 ps-10 text-sm bg-white focus:border-black shadow-xl rounded-xl"
+                placeholder="Username/email"
+                required
+              />
+              <input
+                type="password"
+                id="search"
+                className="block w-full p-3 ps-10 text-sm bg-white focus:border-black shadow-xl rounded-xl"
+                placeholder="Contraseña"
+                required
+              />
+              <div className="flex justify-center">
+                <button className="bg-[#61AF30] text-white px-4 py-2 font-semibold rounded-lg shadow-xl" type="submit">
+                  Ingresa
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </section>
+    
+  );
+}
+
+export default Login;
