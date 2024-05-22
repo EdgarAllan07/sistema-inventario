cb52ba870c4e2b8d7a50447130424a637cd98dad
Edgar Saravia <edgar.saravia@zebedee.io>
2024-05-21 23:01:15 -0600

    cambiando la base de datos

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -26,7 +26,7 @@ function Table() {
         <tbody>
             <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                 <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Apple MacBook Pro 17"
+                    Apple MacBook Pro 17
                 </th>
                 <td class="px-6 py-4">
                     Silver
